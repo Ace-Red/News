2f737b2c3157f78c8d9a4deb04f1ce878c17a02b
Nikita Sakun <NikitaSakun1@gmail.com>
2023-07-02 20:51:40 +0300

    Complete Project

---
+++ H4X0R News/Models/PostData.swift
@@ -0,0 +1,25 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Nikita on 15.06.2023.
+//
+
+import Foundation
+
+struct Results: Decodable{
+    let hits: [Post]
+    
+    
+}
+
+struct Post: Decodable, Identifiable{
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+    
+}
